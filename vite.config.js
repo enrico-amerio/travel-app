999d8f1fca5a87866f4e0678c4be0e89d7843fc6
enrico-amerio <amerio.enrico95@gmail.com>
2024-09-01 13:51:48 +0200

    [fix]

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,4 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/', // Configura il percorso base per il tuo progetto
-  build: {
-    outDir: 'dist', // Directory di output per i file di build
-  },
-  server: {
-    historyApiFallback: true // Gestisce il routing lato client in sviluppo
-  }
 })
